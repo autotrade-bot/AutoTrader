ca2d6a8983641556e8c1676e306d89f87d4a0aa5
takuya <info@yujilow.net>
2017-12-22 17:04:54 +0900

    fix api

--- autotrade/driver/api/bitflyer.py
+++ autotrade/driver/api/bitflyer.py
@@ -2,30 +2,36 @@ import pybitflyer
 import os
 
 class BitflyerApiDriver():
-    def buy(self, price=0):
+
+    def execute(self, strategy_result):
+        getattr(self, strategy_result.get('action'))(strategy_result.get('amount'))
+    
+    def buy(self, amount):
         currency = "BTC"
         api = pybitflyer.API(api_key=os.env['BITFLYER_API_KEY'], api_secret=os.env['BITFLYER_API_SECRET'])
         api.sendchildorder(product_code="%s_JPY" % currency,
                        child_order_type="MARKET",
                        side="BUY",
-                       size=self.jpy_to_size(currency, price),
+                       size=self.jpy_to_size(currency, amount),
                        minute_to_expire=10000,
                        time_in_force="GTC"
                        )
         return
     
-    def sell(self, size=0):
+    def sell(self, amount):
         currency = "BTC"
         api = pybitflyer.API(api_key=os.env['BITFLYER_API_KEY'], api_secret=os.env['BITFLYER_API_SECRET'])
         api.sendchildorder(product_code="%s_JPY" % currency,
                        child_order_type="MARKET",
                        side="SELL",
-                       size=size,
+                       size=amount,
                        minute_to_expire=10000,
                        time_in_force="GTC"
                        )
         return
-    
+
+    def nothing(self):
+        pass
     
     def jpy_to_size(self, currency, price):
         api = pybitflyer.API()
