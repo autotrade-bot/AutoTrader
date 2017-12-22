9c24d3ce1515b699ceef41e870219eec4bdec36d
takuya <info@yujilow.net>
2017-12-22 16:45:30 +0900

    add

--- autotrade/worker.py
+++ autotrade/worker.py
@@ -2,23 +2,22 @@ from oslo_utils import importutils
 import json
 class AutotradeWorker():
     def __init__(self):
-        pass
+        self.conf = self.load_conf('conf.json')
+        self.drivers = self.load_driver(self.conf)
 
     def load_driver(self, conf):
         drivers = {}
         driver_info = conf['driver']
         for key, val in driver_info.items():
-            drivers[key] = importutils.import_class(val)
+            drivers[key] = importutils.import_class(val)()
         return drivers
-
-    def start(self):
-        conf = self.load_conf('conf.json')
-        drivers = self.load_driver(conf)
-        print(drivers)
 
     def load_conf(self, conf_path):
         return json.load(open(conf_path))
 
+    def execute(self):
+        action = self.drivers['strategy'].get_next_action(self.drivers['api'])
+        self.drivers['api'].execute(action)
 
 worker = AutotradeWorker()
-worker.start()
+worker.execute()
