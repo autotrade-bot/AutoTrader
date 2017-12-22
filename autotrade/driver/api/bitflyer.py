a7c0c96ff800b22b857e9c5e0aadf76f367593cc
takuya <info@yujilow.net>
2017-12-22 17:31:23 +0900

    add statuscode

--- autotrade/driver/api/bitflyer.py
+++ autotrade/driver/api/bitflyer.py
@@ -3,35 +3,43 @@ import os
 
 class BitflyerApiDriver():
 
+    def get_balance(self):
+        api = pybitflyer.API(api_key=os.env['API_KEY'], api_secret=os.env['API_SECRET'])
+        result = {}
+        for balance in api.getbalance():
+            result[balance.get("currency_code")] = balance.get("available")
+        return result
+
     def execute(self, strategy_result):
-        getattr(self, strategy_result.get('action'))(strategy_result.get('amount'))
+        return getattr(self, strategy_result.get('action'))(strategy_result.get('amount'))
     
     def buy(self, amount):
         currency = "BTC"
         api = pybitflyer.API(api_key=os.env['BITFLYER_API_KEY'], api_secret=os.env['BITFLYER_API_SECRET'])
-        api.sendchildorder(product_code="%s_JPY" % currency,
+        r = api.sendchildorder(product_code="%s_JPY" % currency,
                        child_order_type="MARKET",
                        side="BUY",
                        size=self.jpy_to_size(currency, amount),
                        minute_to_expire=10000,
                        time_in_force="GTC"
                        )
-        return
+        return r.status_code
     
     def sell(self, amount):
         currency = "BTC"
         api = pybitflyer.API(api_key=os.env['BITFLYER_API_KEY'], api_secret=os.env['BITFLYER_API_SECRET'])
-        api.sendchildorder(product_code="%s_JPY" % currency,
+        r = api.sendchildorder(product_code="%s_JPY" % currency,
                        child_order_type="MARKET",
                        side="SELL",
                        size=amount,
                        minute_to_expire=10000,
                        time_in_force="GTC"
                        )
-        return
+        return r.status_code
 
     def nothing(self):
-        pass
+        print("nothing")
+        return 200
     
     def jpy_to_size(self, currency, price):
         api = pybitflyer.API()
--- autotrade/driver/strategy/exec.py
+++ autotrade/driver/strategy/exec.py
@@ -1,3 +1,10 @@
 class StrategyDriver():
     def __init__(self):
         pass
+
+    def get_neext_action(self):
+        na = {
+                "action": "buy",
+                "amount": "100"
+                }
+        return na
