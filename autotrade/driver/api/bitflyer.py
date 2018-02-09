28465e4227600687532b0a995dd9f470cf80a607
takuya <info@yujilow.net>
2018-02-09 17:12:58 +0900

    mod for multiple collaterals

--- autotrade/driver/api/bitflyer.py
+++ autotrade/driver/api/bitflyer.py
@@ -16,11 +16,12 @@ class BitflyerFxApiDriver():
         positions = api.getpositions(product_code="FX_BTC_JPY")
         collateral = api.getcollateral()
         if len(positions) > 0:
-            pos = positions.pop()
-            if pos["side"] == "BUY":
-                size = float(pos['size'])
-            else:
-                size = float(pos['size']) * -1
+            size = 0.0
+            for pos in positions:
+                if pos["side"] == "BUY":
+                    size += float(pos['size'])
+                else:
+                    size += float(pos['size']) * -1
             result["BTC"] = size
             result["JPY"] = collateral['collateral']
         else:
@@ -61,7 +62,6 @@ class BitflyerFxApiDriver():
         api = pybitflyer.API()
         ticker =  api.ticker(product_code="FX_BTC_JPY")
         return ticker.get('best_bid')
-    
     """
     通知用メソッド
     params: なし
@@ -69,7 +69,7 @@ class BitflyerFxApiDriver():
     """
     def collect_data(self, action_json):
         api = pybitflyer.API(api_key=os.environ['API_KEY'], api_secret=os.environ['API_SECRET'])
-        board = api.board(product_code="FX_BTC_JPY") 
+        board = api.board(product_code="FX_BTC_JPY")
         positions = api.getpositions(product_code="FX_BTC_JPY")
         if len(positions) > 0:
             position = positions.pop()
