ab9d546d7006738530f77ae453a9574fae4582a9
takuya TAKAHASHI <adgjmbehkncfilo1234@gmail.com>
2018-02-14 12:35:47 +0900

    mod to use jp timezone

--- autotrade/make_chart.py
+++ autotrade/make_chart.py
@@ -1,19 +1,11 @@
-from pubnub.callbacks import SubscribeCallback
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime, and_
 from datetime import datetime
-from pubnub.enums import PNStatusCategory
-from pubnub.pnconfiguration import PNConfiguration
-from pubnub.pubnub_tornado import PubNubTornado
-from pubnub.pnconfiguration import PNReconnectionPolicy
-
-config = PNConfiguration()
-config.subscribe_key = 'sub-c-52a9ab50-291b-11e5-baaa-0619f8945a4f'
-config.reconnect_policy = PNReconnectionPolicy.LINEAR
-pubnub = PubNubTornado(config)
-
-from tornado import gen
-from dateutil.parser import parse
 from pytz import timezone
+from time import sleep
+import json
+import subprocess
+
+
 class SQLiteStoreDriver():
     def __init__(self):
         engine = create_engine('mysql+pymysql://root:password@mysql/autotrade?charset=utf8', echo=True)
@@ -40,59 +32,19 @@ class SQLiteStoreDriver():
                 close=cl,
                 created_at=created_at)
 
-
-
-@gen.coroutine
-def main(channels):
+def main():
     sqlite = SQLiteStoreDriver()
-    class BitflyerSubscriberCallback(SubscribeCallback):
-        price_list = []
-        pre_d = None
-        def presence(self, pubnub, presence):
-            pass  # handle incoming presence data
-
-        def status(self, pubnub, status):
-            if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
-                pass  # This event happens when radio / connectivity is lost
-
-            elif status.category == PNStatusCategory.PNConnectedCategory:
-                # Connect event. You can do stuff like publish, and know you'll get it.
-                # Or just use the connected event to confirm you are subscribed for
-                # UI / internal notifications, etc
-                pass
-            elif status.category == PNStatusCategory.PNReconnectedCategory:
-                pass
-                # Happens as part of our regular operation. This event happens when
-                # radio / connectivity is lost, then regained.
-            elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
-                pass
-                # Handle message decryption error. Probably client configured to
-                # encrypt messages and on live data feed it received plain text.
-
-        def message(self, pubnub, message):
-            # Handle new message stored in message.message
-            # メインの処理はここで書きます
-            # 登録したチャンネルからメッセージ(価格の変化など)がくるたび、この関数が呼ばれます
-            # {'id': 113370452, 'side': 'SELL', 'price': 1922595.0, 'size': 0.1, 'exec_date': '2018-01-14T13:41:19.0924133Z', 'buy_child_order_acceptance_id': 'JRF20180114-224113-653741', 'sell_child_order_acceptance_id': 'JRF20180114-134118-859029'}
-            for m in message.message:
-                d = parse(m.get('exec_date')).astimezone(timezone('Asia/Tokyo'))
-                # print('{0},{1},{2}'.format(d, m.get('side'), m.get('price')))
-                if self.pre_d is not None and self.pre_d.minute < d.minute:
-                    sqlite.put(self.price_list[0], max(self.price_list), min(self.price_list), self.price_list[-1], self.pre_d)
-                    print(self.price_list[0], max(self.price_list), min(self.price_list), self.price_list[-1])
-                    self.price_list = []
-                else:
-                    self.price_list.append(m.get('price'))
-                self.pre_d = d
-
-
-    listener = BitflyerSubscriberCallback()
-    pubnub.add_listener(listener)
-    pubnub.subscribe().channels(channels).execute()
+    p = subprocess.Popen('curl https://api.cryptowat.ch/markets/bitflyer/btcfxjpy/ohlc?periods=60', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    last_chart = json.loads(p.stdout.read().decode('utf-8'))['result']['60'][-1]
+    open_price = last_chart[1]
+    high_price = last_chart[2]
+    low_price = last_chart[3]
+    close_price = last_chart[4]
+    created_at = datetime.fromtimestamp(last_chart[0]).astimezone(timezone('Asia/Tokyo'))
+    sqlite.put(open_price, high_price, low_price, close_price, created_at)
 
 if __name__ == '__main__':
-    channels = [
-        'lightning_executions_FX_BTC_JPY',
-    ]
-    main(channels)
-    pubnub.start()
+    while True:
+        main()
+        sleep(60)
